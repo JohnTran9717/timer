e1b5125dc32e3606b1550abd6b7acab4373b31d4
JohnTran9717 <77471396+JohnTran9717@users.noreply.github.com>
2021-07-22 11:41:08 -0400

    Update Count.js
    added stop conditional if statement to stop multiple timer functions

--- countdown timer/Count.js
+++ countdown timer/Count.js
@@ -6,63 +6,68 @@ let h = document.getElementById("hour");
 let m = document.getElementById("minute");
 let s = document.getElementById("second")
 let hs = document.getElementById("hundredthsecond");
-
-let startTime= null;
-
+let startTime = null;
+let stop = 1;
 function timer(){
-    if( d.value == 0 && h.value ==0 && m.value==0 && s.value==0 && hs.value == 0){
-    d.value=0;
-    h.value=0;
-    m.value=0;
-    s.value=0;
-    hs.value=0;
-    stopTimer()
-    } else if (hs.value !=0){
+    if( d.value==0 && h.value==0 && m.value==0 && s.value==0 && hs.value==0){
+    d.value= 0;
+    h.value= 0;
+    m.value= 0;
+    s.value= 0;
+    hs.value= 0;
+    console.log(stop)
+    } else if (hs.value!=0){
         hs.value--;
-    } else if (s.value !=0 && hs.value==0){
-        hs.value= 99;
+    } else if (s.value!=0 && hs.value==0){
+        hs.value=99;
         s.value--;
-    } else if (m.value !=0 && s.value==0 && hs.value==0){
-        hs.value= 99;
-        s.value= 59;
+    } else if (m.value!=0 && s.value==0 && hs.value==0){
+        hs.value=99;
+        s.value=59;
         m.value--;
-    } else if (h.value !=0 && m.value==0 && s.value==0 && hs.value==0){
-        hs.value= 99;
-        s.value= 59;
-        m.value= 59;
+    } else if (h.value!=0 && m.value==0 && s.value==0 && hs.value==0){
+        hs.value=99;
+        s.value=59;
+        m.value=59;
         h.value--;
-    } else if (d.value !=0 && h.value ==0 && m.value==0 && s.value==0 && hs.value==0){
-        hs.value= 99;
-        s.value= 59;
-        m.value= 59;
-        h.value= 23;
+    } else if (d.value!=0 && h.value==0 && m.value==0 && s.value==0 && hs.value==0){
+        hs.value=99;
+        s.value=59;
+        m.value=59;
+        h.value=23;
         d.value--;
     }
 }
-function stopTimer() {
+function stopTimer(){ 
+    if (stop == 0){
+    console.log("test4");
+    stop = 1;
+    console.log(stop);
     clearInterval(startTime);
-    console.log("test4")
-}
-
-start.addEventListener("click", function(){
-    console.log("test1")
-    function startTimer() {
-        startTime = setInterval(function(){
-            timer();
-        }, 10)
     }
-    startTimer()
+}
+function startTimer(){
+    if(stop == 1){
+    stop = 0;
+    console.log(stop);
+    startTime = setInterval(function(){
+        timer();}, 10)
+    }
+};
+start.addEventListener("click", function(){
+        console.log("test1")
+        startTimer()
 });
 pause.addEventListener("click", function (){
-    console.log("test2")
-    stopTimer()
-})
+        console.log("test2");
+        stopTimer();
+});
 reset.addEventListener("click", function(){
-    console.log("test3")
-    d.value=0;
-    h.value=0;
-    m.value=0;
-    s.value=0;
-    hs.value=0;
-    stopTimer()
+        console.log("test3")
+        stopTimer();
+        d.value=0;
+        h.value=0;
+        m.value=0;
+        s.value=0;
+        hs.value=0;
 });
