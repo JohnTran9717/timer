68cafd7a0505e64ad745d82c187bbb4e5fa7ee53
JohnTran9717 <77471396+JohnTran9717@users.noreply.github.com>
2021-07-23 13:57:43 -0400

    removed loop
    open loop removed and cade reorganized

--- countdown timer/Count.js
+++ countdown timer/Count.js
@@ -7,53 +7,8 @@ let m = document.getElementById("minute");
 let s = document.getElementById("second")
 let hs = document.getElementById("hundredthsecond");
 let startTime = null;
-let stop = 1;
-function timer(){
-    if( d.value==0 && h.value==0 && m.value==0 && s.value==0 && hs.value==0){
-    d.value= 0;
-    h.value= 0;
-    m.value= 0;
-    s.value= 0;
-    hs.value= 0;
-    console.log(stop)
-    } else if (hs.value!=0){
-        hs.value--;
-    } else if (s.value!=0 && hs.value==0){
-        hs.value=99;
-        s.value--;
-    } else if (m.value!=0 && s.value==0 && hs.value==0){
-        hs.value=99;
-        s.value=59;
-        m.value--;
-    } else if (h.value!=0 && m.value==0 && s.value==0 && hs.value==0){
-        hs.value=99;
-        s.value=59;
-        m.value=59;
-        h.value--;
-    } else if (d.value!=0 && h.value==0 && m.value==0 && s.value==0 && hs.value==0){
-        hs.value=99;
-        s.value=59;
-        m.value=59;
-        h.value=23;
-        d.value--;
-    }
-}
-function stopTimer(){ 
-    if (stop == 0){
-    console.log("test4");
-    stop = 1;
-    console.log(stop);
-    clearInterval(startTime);
-    }
-}
-function startTimer(){
-    if(stop == 1){
-    stop = 0;
-    console.log(stop);
-    startTime = setInterval(function(){
-        timer();}, 10)
-    }
-};
+let stopOrStart = 0;
+//event listeners
 start.addEventListener("click", function(){
         console.log("test1")
         startTimer()
@@ -71,3 +26,45 @@ reset.addEventListener("click", function(){
         s.value=0;
         hs.value=0;
 });
+//functions
+function startTimer(){
+        if(stopOrStart == 0){
+        stopOrStart = 1;
+        console.log(stopOrStart);
+        startTime = setInterval(function(){timer()}, 10);
+        };
+        function timer(){console.log("test4")
+            if(hs.value!=0){
+                hs.value--;
+            } else if (s.value!=0 && hs.value==0){
+                hs.value=99;
+                s.value--;
+            } else if (m.value!=0 && s.value==0 && hs.value==0){
+                hs.value=99;
+                s.value=59;
+                m.value--;
+            } else if (h.value!=0 && m.value==0 && s.value==0 && hs.value==0){
+                hs.value=99;
+                s.value=59;
+                m.value=59;
+                h.value--;
+            } else if (d.value!=0 && h.value==0 && m.value==0 && s.value==0 && hs.value==0){
+                hs.value=99;
+                s.value=59;
+                m.value=59;
+                h.value=23;
+                d.value--;
+            }else if (d.value==0 && h.value==0 && m.value==0 && s.value==0 && hs.value==0){
+                console.log("test5");
+                stopTimer();
+        }
+    };
+};
+function stopTimer(){ 
+    if (stopOrStart == 1 ){
+    console.log("test6");
+    stopOrStart = 0;
+    console.log(stopOrStart);
+    clearInterval(startTime);
+    };
+};
